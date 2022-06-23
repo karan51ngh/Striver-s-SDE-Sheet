94f5837b9e65a158b3dc0ada484a6cd52809e9fd
karan51ngh <karansingh9535@gmail.com>
2022-06-23 13:00:33 +0530

    Set Matrix Zeroes

---
+++ Day1/1.setMatrixZeros.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+public:
+	int NUM = 1000000007;
+	void setZeroes(vector<vector<int>> &matrix)
+	{
+		int n = matrix.size();
+		int m = matrix[0].size();
+
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				if (matrix[i][j] == 0)
+				{
+					for (int k = 0; k < n; k++)
+					{
+						if (matrix[k][j] != 0)
+							matrix[k][j] = NUM;
+					}
+					for (int k = 0; k < m; k++)
+					{
+						if (matrix[i][k] != 0)
+							matrix[i][k] = NUM;
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				if (matrix[i][j] == NUM)
+					matrix[i][j] = 0;
+	}
+};
